d04deddbb7ce18ebf271445b8c3ebfaa4d4d378d
jdifek <givotkov11@gmail.com>
2025-07-12 10:35:38 +0200

    2 check

--- app/[lang]/mfo/[company]/page.tsx
+++ app/[lang]/mfo/[company]/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Image from "next/image";
-import DetailsText from "@/app/components/DetailsText";
 import OftenQuestions from "@/app/components/OftenQuestions";
 import Link from "next/link";
 import AboutButtons from "@/app/components/AboutButtons";
